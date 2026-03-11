6e0f6756aed7ec10675441394fc8863d3abbb18c
MECOAN <mario_15200@hotmail.com>
2026-03-11 13:29:49 -0600

    Implement full CRUD lifecycle for Sptify playlist, and update the README

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Spotify-Api-Testing-Engine
-Test automation engine for the Spotify API. Implements OAuth 2.0 flows, contract validation (JSON Schema), and POJO manipulation using Java 17 and RestAssured.
+# Spotify API Testing Engine 🎵
+
+A robust, enterprise-level test automation framework for the Spotify Web API. This project validates the core
+functionalities of the Spotify API using modern automation practices, focusing on full CRUD lifecycles, stateful
+testing, and secure OAuth 2.0 token management.
+
+## 🚀 Tech Stack
+
+* **Language:** Java 17
+* **API Testing:** REST Assured
+* **Testing Framework:** TestNG
+* **Data Binding (POJOs):** Jackson & Lombok
+* **Build Tool:** Maven
+
+## 🎯 Key Features
+
+* **OAuth 2.0 Automation:** Automated generation and caching of Bearer tokens using `refresh_token` grants.
+* **E2E CRUD Lifecycle:** Implements stateful testing to Create, Read, Update, and Unfollow (Delete) Spotify Playlists
+  sequentially.
+* **State Verification:** Validates `PUT` requests by immediately performing `GET` requests to ensure data persistence.
+* **Soft Assertions:** Utilizes TestNG `SoftAssert` to validate multiple JSON fields simultaneously without aborting the
+  test prematurely.
+* **Singleton Configuration:** Centralized `ConfigLoader` to securely manage environment variables and credentials.
+
+## 🏗️ Project Architecture
+
+The framework follows a modular and scalable design:
+
+* `api/`: Contains route definitions and HTTP methods (`PlaylistApi`, `AccountApi`) and the base configurations (
+  `SpecBuilder`).
+* `models/`: Contains the POJOs (Plain Old Java Objects) built with Lombok for serializing and deserializing JSON
+  payloads.
+* `tests/`: Contains the TestNG execution classes with strict priority dependencies.
+* `utils/`: Contains helper classes for Token Management and Configuration loading.
+
+## ⚙️ Setup and Execution
+
+### 1. Prerequisites
+
+* Java JDK 17+
+* Maven installed
+* A Spotify Developer Account (to obtain Client ID and Client Secret)
+
+### 2. Configuration
+
+To protect sensitive data, credentials are not stored in the repository.
+
+1. Clone the repository.
+2. Navigate to `src/main/resources/`.
+3. Copy the `config.properties.example` file and rename it to `config.properties`.
+4. Fill in your Spotify Developer credentials in the new `config.properties` file:
+   ```properties
+   client_id=YOUR_CLIENT_ID
+   client_secret=YOUR_CLIENT_SECRET
+   refresh_token=YOUR_REFRESH_TOKEN
+   user_id=YOUR_SPOTIFY_USER_ID
+   ...
+
+### 3. Running the Tests
+
+   ```properties
+    mvn clean test
--- src/test/java/com/mario/spotify/tests/PlaylistTests.java
+++ src/test/java/com/mario/spotify/tests/PlaylistTests.java
@@ -4,20 +4,28 @@ import com.mario.spotify.api.PlaylistApi;
 import com.mario.spotify.models.Playlist;
 import com.mario.spotify.utils.ConfigLoader;
 import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class PlaylistTests {
 
-    @Test
+    private String playlistId;
+    private PlaylistApi playlistApi;
+    private String expectedPlaylistName = "Test Playlist";
+    private String expectedDescription = "Description for Test Playlist";
+    private Boolean expectedPublic = true;
+    private String expectedUserId = ConfigLoader.getInstance().getUserId();
+    private String expectedType = "playlist";
+    private int expectedItems = 0;
+
+    @BeforeClass
+    public void setup() {
+        playlistApi = new PlaylistApi();
+    }
+
+    @Test(priority = 1)
     public void testCreatePlaylist() {
-        PlaylistApi playlistApi = new PlaylistApi();
-        String expectedPlaylistName = "Test Playlist";
-        String expectedDescription = "Description for Test Playlist";
-        Boolean expectedPublic = true;
-        String expectedUserId = ConfigLoader.getInstance().getUserId();
-        String expectedType = "playlist";
-        int expectedItems = 0;
 
         Playlist playlist = Playlist
                 .builder()
@@ -32,6 +40,7 @@ public class PlaylistTests {
                 .then()
                 .statusCode(201);
 
+        playlistId = response.path("id");
         String actualName = response.path("name");
         String actualDescription = response.path("description");
         Boolean actualPublic = response.path("public");
@@ -61,7 +70,99 @@ public class PlaylistTests {
                 "The total items in the Playlist is: " + actualTotalItems + " but should be: " + expectedItems);
 
         softAssert.assertAll();
-
     }
 
+    @Test(priority = 2, dependsOnMethods = "testCreatePlaylist")
+    public void testGetPlaylist() {
+        Response response = playlistApi.getPlaylist(playlistId);
+        response
+                .then()
+                .statusCode(200);
+        String actualName = response.path("name");
+        String actualDescription = response.path("description");
+        Boolean actualPublic = response.path("public");
+        String actualType = response.path("type");
+        String actualOwnerId = response.path("owner.id");
+        int actualTotalItems = response.path("items.total");
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(actualName,
+                expectedPlaylistName,
+                "The name of the Playlist is: " + actualName + " but should be: " + expectedPlaylistName);
+        softAssert.assertEquals(actualDescription,
+                expectedDescription,
+                "The description of the Playlist is: " + actualDescription + " but should be: " + expectedDescription);
+        softAssert.assertEquals(actualPublic,
+                expectedPublic,
+                "The public status of the Playlist is: " + actualPublic + " but should be: " + expectedPublic);
+        softAssert.assertEquals(actualType,
+                expectedType,
+                "The type of the Playlist is: " + actualType + " but should be: " + expectedType);
+        softAssert.assertEquals(actualOwnerId,
+                expectedUserId,
+                "The actual User id of the Playlist is: " + actualOwnerId + " but should be: " + expectedUserId);
+        softAssert.assertEquals(actualTotalItems,
+                expectedItems,
+                "The total items in the Playlist is: " + actualTotalItems + " but should be: " + expectedItems);
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 3, dependsOnMethods = "testCreatePlaylist")
+    public void testUpdatePlaylist() {
+        String expectedNewName = "Playlist v2";
+        String expectedNewDescription = "Playlist modified";
+
+        Playlist playlist = Playlist
+                .builder()
+                .name(expectedNewName)
+                .description(expectedNewDescription)
+                .isPublic(expectedPublic)
+                .build();
+
+        Response response = playlistApi.updatePlaylist(playlistId, playlist);
+        response.then()
+                .statusCode(200);
+
+        response = playlistApi.getPlaylist(playlistId);
+        response
+                .then()
+                .statusCode(200);
+
+        String actualName = response.path("name");
+        String actualDescription = response.path("description");
+        Boolean actualPublic = response.path("public");
+        String actualType = response.path("type");
+        String actualOwnerId = response.path("owner.id");
+        int actualTotalItems = response.path("items.total");
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(actualName,
+                expectedNewName,
+                "The name of the Playlist is: " + actualName + " but should be: " + expectedNewName);
+        softAssert.assertEquals(actualDescription,
+                expectedNewDescription,
+                "The description of the Playlist is: " + actualDescription + " but should be: " + expectedNewDescription);
+        softAssert.assertEquals(actualPublic,
+                expectedPublic,
+                "The  public status of the Playlist is: " + actualPublic + " but should be: " + expectedPublic);
+        softAssert.assertEquals(actualType,
+                expectedType,
+                "The type of the Playlist is: " + actualType + " but should be: " + expectedType);
+        softAssert.assertEquals(actualOwnerId,
+                expectedUserId,
+                "The actual User id of the Playlist is: " + actualOwnerId + " but should be: " + expectedUserId);
+        softAssert.assertEquals(actualTotalItems,
+                expectedItems,
+                "The total items in  the Playlist is: " + actualTotalItems + " but should be: " + expectedItems);
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 4, dependsOnMethods = "testCreatePlaylist")
+    public void testDeletePlaylist() {
+        Response response = playlistApi.deletePlaylist(playlistId);
+        response
+                .then()
+                .statusCode(200);
+    }
 }
